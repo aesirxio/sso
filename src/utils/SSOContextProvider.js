efcacc2e435ef398f52ee15bc1c576ca1c0c4c6a
Viet Nguyen <viet.nguyen@redweb.dk>
2023-01-06 10:11:50 +0700

    NextJS fix window is not defined

--- src/utils/SSOContextProvider.js
+++ src/utils/SSOContextProvider.js
@@ -21,7 +21,7 @@ export class SSOContextProvider extends React.Component {
 
       const clientSecret =
         process.env.REACT_APP_SSO_CLIENT_SECRET ?? process.env.NEXT_PUBLIC_SSO_CLIENT_SECRET;
-      const queryString = window.location.search;
+      const queryString = typeof window !== 'undefined' && window.location.search;
       const urlParams = new URLSearchParams(queryString);
 
       if (urlParams.get('state') === 'sso') {
@@ -32,7 +32,7 @@ export class SSOContextProvider extends React.Component {
             client_id: clientID,
             client_secret: clientSecret,
           });
-          if (res.data) {
+          if (res.data && typeof window !== 'undefined') {
             window.opener.sso_response = res.data;
             window.close();
           }
@@ -48,7 +48,7 @@ export class SSOContextProvider extends React.Component {
           urlParams.get('token_type') &&
             Object.assign(data, { scope: urlParams.get('token_type') });
 
-          if (Object.keys(data).length) {
+          if (Object.keys(data).length && typeof window !== 'undefined') {
             window.opener.sso_response = data;
             window.close();
           }
@@ -64,7 +64,7 @@ export class SSOContextProvider extends React.Component {
   render() {
     return (
       <SSOContext.Provider value={{ ...this.props.value }}>
-        {window?.opener && <Spinner />}
+        {typeof window !== 'undefined' && window?.opener && <Spinner />}
         {this.props.children}
       </SSOContext.Provider>
     );
