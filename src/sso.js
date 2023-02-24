cf021eae2dd199823abdbf8087d8da110f155709
vladimir <vladimir@r-digital.tech>
2023-02-24 12:46:57 +0200

    reformat

--- src/sso.js
+++ src/sso.js
@@ -1,13 +1,16 @@
 import axios from 'axios';
+
 const aesirxSSO = async () => {
   const endPoint = window.aesirxEndpoint ? window.aesirxEndpoint.replace(/\/+$/, '') : '';
   const clientID = window.aesirxClientID ? window.aesirxClientID : '';
   const clientSecret = window.aesirxClientSecret ? window.aesirxClientSecret : '';
   const ssoState = window.aesirxSSOState ? window.aesirxSSOState : 'sso';
-  const aesirxAllowedLogins = window.aesirxAllowedLogins ? window.aesirxAllowedLogins : ["concordium","metamask","regular"];
+  const aesirxAllowedLogins = window.aesirxAllowedLogins
+    ? window.aesirxAllowedLogins
+    : ['concordium', 'metamask', 'regular'];
   let aesirxSSOLink = `${endPoint}/index.php?option=authorize&api=oauth2&response_type=code&client_id=${clientID}&state=${ssoState}`;
   for (var i = 0; i < aesirxAllowedLogins.length; i++) {
-    aesirxSSOLink += "&login[]=" + aesirxAllowedLogins[i];
+    aesirxSSOLink += '&login[]=' + aesirxAllowedLogins[i];
   }
   const queryString = typeof window !== 'undefined' && window.location.search;
   const urlParams = new URLSearchParams(queryString);
