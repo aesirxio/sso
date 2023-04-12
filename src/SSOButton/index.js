fd3d004b02de89a3d41d35d8ee24f78aeb908de5
Viet <viet@r-digital.tech>
2023-04-12 10:57:19 +0700

    Update code

--- src/SSOButton/index.js
+++ src/SSOButton/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const SSOButton = ({ className, text, onGetData, options }) => {
-  const endPoint =
-    process.env.REACT_APP_ENDPOINT_URL?.replace(/\/+$/, '') ??
-    process.env.NEXT_PUBLIC_ENDPOINT_URL?.replace(/\/+$/, '');
+  const endPoint = process.env.REACT_APP_ENDPOINT_URL
+    ? new URL(process.env.REACT_APP_ENDPOINT_URL)?.origin
+    : new URL(process.env.NEXT_PUBLIC_ENDPOINT_URL)?.origin;
   const clientID = process.env.REACT_APP_SSO_CLIENT_ID ?? process.env.NEXT_PUBLIC_SSO_CLIENT_ID;
   const optionList = options?.length ? options?.map((item) => `&login[]=${item}`).join('') : '';
   const popupLink = `${endPoint}/index.php?option=authorize&api=oauth2&response_type=code&client_id=${clientID}&state=sso${optionList}`;
