6cae40934338f8812db53cb98f671dee8a486b2a
Toan Pham <toan@redweb.dk>
2023-05-30 15:10:05 +0700

    udpate stup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   format: ['esm'],
   sourcemap: true,
-  entryPoints: ['src/index.tsx', 'src/sso.tsx'],
   outDir: 'build',
+  entry: ['src/index.{ts,tsx}', 'src/**/*.{ts,tsx}', '!src/**/*.test.{ts,tsx}', '!src/**/*.d.ts'],
   external: ['react', 'react-dom'],
   bundle: true,
   minify: true,
