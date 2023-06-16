72b4dba099cdc875fe5f31a5793ef1e930c96276
Toan Pham <toan@redweb.dk>
2023-06-16 11:52:08 +0700

    udapte

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  format: ['esm'],
+  format: ['cjs', 'esm'],
   sourcemap: true,
   outDir: 'build',
   entry: ['src/index.{ts,tsx}', 'src/**/*.{ts,tsx}', '!src/**/*.test.{ts,tsx}', '!src/**/*.d.ts'],
   external: ['react', 'react-dom'],
   bundle: true,
   minify: true,
+  target: 'es2020',
   clean: true,
   dts: true,
   loader: {
