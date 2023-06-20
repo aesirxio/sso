94fd7007cd01774e30f4656aea2e6d52c81ed0bb
Toan Pham <toan@redweb.dk>
2023-06-20 13:56:36 +0700

    update

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   sourcemap: true,
   outDir: 'build',
   entry: ['src/index.{ts,tsx}', 'src/**/*.{ts,tsx}', '!src/**/*.test.{ts,tsx}', '!src/**/*.d.ts'],
