c8af2642bd23358e9d6495a9bd4bb2e178c57b57
Toan Pham <toan@redweb.dk>
2023-06-21 15:35:31 +0700

    udapte stup

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,22 +1,48 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-  format: ['esm'],
-  sourcemap: true,
-  outDir: 'build',
-  entry: ['src/index.{ts,tsx}', 'src/**/*.{ts,tsx}', '!src/**/*.test.{ts,tsx}', '!src/**/*.d.ts'],
-  external: ['react', 'react-dom'],
-  bundle: true,
-  minify: true,
-  target: 'es2020',
-  clean: true,
-  dts: true,
-  loader: {
-    '.js': 'jsx',
-  },
-  outExtension() {
+const tsupConfig = defineConfig([
+  {
+    format: ['esm'],
+    sourcemap: true,
+    outDir: 'build/lib',
+    entry: ['src/index.{ts,tsx}', 'src/**/*.{ts,tsx}', '!src/sso.{ts,tsx}','!src/**/*.d.ts'],
+    external: ['react', 'react-dom'],
+    bundle: true,
+    minify: true,
+    target: 'es2020',
+    clean: true,
+    dts: true,
+    loader: {
+      '.js': 'jsx',
+    },
+ 
+ outExtension() {
+   return {
+     js: `.js`,
+   };
+ },
+ },
+ {
+    format: ['esm'],
+    sourcemap:false,
+    outDir: 'build',
+    entry: ['src/sso.{ts,tsx}'],
+    external: ['react', 'react-dom'],
+    bundle: true,
+    minify: true,
+    target: 'es2020',
+    clean: true,
+    dts: false,
+    loader: {
+      '.js': 'jsx',
+    },
+
+    outExtension() {
     return {
       js: `.js`,
     };
+    },
   },
-});
+]);
+
+export default tsupConfig;
