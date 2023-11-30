ce75063be97f2e10fd7f5584bbd47036ef6f1201
Viet <viet@r-digital.tech>
2023-11-30 14:29:25 +0700

    Fix android

--- src/SSOButton/Providers/Concordium/connect.tsx
+++ src/SSOButton/Providers/Concordium/connect.tsx
@@ -1,7 +1,7 @@
 import { BROWSER_WALLET, WALLET_CONNECT } from './config';
 import React from 'react';
 import concordium_logo from '../../images/concordium.png';
-import { isMobile, isDesktop, osName, OsTypes } from 'react-device-detect';
+import { isMobile, isDesktop, osName, OsTypes, isAndroid } from 'react-device-detect';
 import { Col, Row } from 'react-bootstrap';
 
 const ConnectConcordium = ({
@@ -17,7 +17,7 @@ const ConnectConcordium = ({
       <div className="">
         <Row>
           <Col lg={`${noLogin ? '6' : '12'}`}>
-            {isDesktop && (
+            {isDesktop ? (
               <button
                 disabled={isConnecting}
                 className={`btn btn-dark btn-concordium fw-medium text-nowrap py-2 px-4 fs-18 lh-sm text-white d-flex align-items-center justify-content-start w-100 ${
@@ -46,34 +46,37 @@ const ConnectConcordium = ({
                   </>
                 )}
               </button>
+            ) : isAndroid ? (
+              <>
+                <button
+                  className={`btn btn-dark btn-concordium flex-grow-1 fw-medium py-2 px-4 fs-18 lh-sm text-white d-flex align-items-center justify-content-start w-100`}
+                  onClick={() => handleOnConnect(WALLET_CONNECT, 'walletconnect')}
+                >
+                  {!activeConnectorError && activeConnectorType && !activeConnector ? (
+                    <span
+                      className="spinner-border spinner-border-sm me-1"
+                      role="status"
+                      aria-hidden="true"
+                    ></span>
+                  ) : (
+                    <>
+                      <div className="d-flex text-start align-items-center">
+                        <p className="mb-0 fs-18 text-nowrap pe-3 me-3 border-end text-nowrap">
+                          QR Code
+                        </p>
+                        <p className="mb-0 fs-12">Concordium or CryptoX Mobile Wallets</p>
+                      </div>
+                    </>
+                  )}
+                </button>
+              </>
+            ) : (
+              <></>
             )}
           </Col>
-          {/* <Col lg={`${noLogin ? '6' : '12'}`}>
-            <button
-              className={`btn btn-dark btn-concordium flex-grow-1 fw-medium py-2 px-4 fs-18 lh-sm text-white d-flex align-items-center justify-content-start w-100`}
-              onClick={() => handleOnConnect(WALLET_CONNECT, 'walletconnect')}
-            >
-              {!activeConnectorError && activeConnectorType && !activeConnector ? (
-                <span
-                  className="spinner-border spinner-border-sm me-1"
-                  role="status"
-                  aria-hidden="true"
-                ></span>
-              ) : (
-                <>
-                  <div className="d-flex text-start align-items-center">
-                    <p className="mb-0 fs-18 text-nowrap pe-3 me-3 border-end text-nowrap">
-                      QR Code
-                    </p>
-                    <p className="mb-0 fs-12">Concordium or CryptoX Mobile Wallets</p>
-                  </div>
-                </>
-              )}
-            </button>
-          </Col> */}
         </Row>
       </div>
-      {isMobile && (
+      {isMobile && isAndroid && (
         <div className="bg-success-light p-3 mt-3 fs-14 text-start">
           <p className="mb-1 fw-bold">Note for Concordium Wallet:</p>
           <p className="mb-0">
