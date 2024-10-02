fe43774b9792ce79b39b90042b99f9e07dc289a2
Viet <viet@r-digital.tech>
2024-10-02 14:14:57 +0700

    Update prettier

--- src/SSOButton/Providers/CreateAccount/index.tsx
+++ src/SSOButton/Providers/CreateAccount/index.tsx
@@ -302,19 +302,19 @@ const CreateAccount = ({
           username: data[`field${registerForm.email}_1_email`]
             ? data[`field${registerForm.email}_1_email`]
             : Object.keys(socialType)?.length
-            ? `${socialType?.id}`
-            : `${walletState?.accountAddress}`,
+              ? `${socialType?.id}`
+              : `${walletState?.accountAddress}`,
           password: passwordGenerate,
           email: data[`field${registerForm.email}_1_email`]
             ? data[`field${registerForm.email}_1_email`]
             : Object.keys(socialType)?.length
-            ? `${socialType?.id}@aesirx.io`
-            : `${walletState?.accountAddress}@aesirx.io`,
+              ? `${socialType?.id}@aesirx.io`
+              : `${walletState?.accountAddress}@aesirx.io`,
           organisation: data[`field${registerForm.email}_1_email`]
             ? data[`field${registerForm.email}_1_email`]
             : Object.keys(socialType)?.length
-            ? `${socialType?.id}`
-            : `${walletState?.accountAddress}`,
+              ? `${socialType?.id}`
+              : `${walletState?.accountAddress}`,
           block: 0,
           ...(walletState?.wallet === 'concordium'
             ? { wallet_concordium: walletState?.accountAddress }
@@ -347,8 +347,8 @@ const CreateAccount = ({
               [`field${registerForm.email}_1[email]`]: data[`field${registerForm.email}_1_email`]
                 ? data[`field${registerForm.email}_1_email`]
                 : Object.keys(socialType).length
-                ? `${socialType?.id}@aesirx.io`
-                : `${walletState?.accountAddress}@aesirx.io`,
+                  ? `${socialType?.id}@aesirx.io`
+                  : `${walletState?.accountAddress}@aesirx.io`,
               [`field${registerForm.organization}_1`]: data[`field${registerForm.organization}_1`],
               [`field${registerForm.message}_1`]: data[`field${registerForm.message}_1`],
               [`field${registerForm.order_id}_1`]: data[`field${registerForm.order_id}_1`] ?? '',
